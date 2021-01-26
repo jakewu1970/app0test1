f44211236dc04e80a0a67cbb9fe5e526f39f7bd3
coder-dog <911485123@qq.com>
2021-01-26 10:29:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+//token 758dfffe462b27d8e7c8d16c870e3d4e3e82349b
